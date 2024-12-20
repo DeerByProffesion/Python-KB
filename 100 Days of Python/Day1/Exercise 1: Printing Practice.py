628cfa98825014661f37e1bf43d5771980d1fb46
DeerByProffesion <m.d.sobieraj@gmail.com>
2024-12-20 08:38:40 +0100

    Python Exercises Day 1

---
+++ 100 Days of Python/Day1/Exercise 1: Printing Practice.py
@@ -0,0 +1,17 @@
+# Print below 5 lines of code
+#
+# 1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+# 2. Knead the dough for 10 minutes.
+# 3. Add 3g of Salt.
+# 4. Leave to rise for 2 hours.
+# 5. Bake at 200 degrees C for 30 minutes.
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.\n")
+
+# Or as an alternative
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
---
+++ 100 Days of Python/Day1/Project: Band Name Generator.py
@@ -0,0 +1,12 @@
+# Band Naming Generator
+
+# This project should consist below:
+
+# 1. Welcome prompt.
+# 2. Question that saves the user input - "What's the name of the city you grw up in?".
+# 3. Question that saves the user input - "What's your pet's name?".
+# 4. Prompt Output combining both values with space inbetween telling the name of the band.
+
+cityName = input("What's the name of the city you grew up in?\n")
+petName = input("what's your pet's name?\n")
+print(f"Your band name could be {cityName} {petName}")
